169640b8ec198a74f2638e41d53c14a8e3375d2c
RifqiNF <rifqi2006bdg@gmail.com>
2026-07-28 23:31:05 +0700

    balik width dan height di tes roi

--- test_roi.py
+++ test_roi.py
@@ -14,7 +14,7 @@ def main():
     global ROI_X, ROI_Y, ROI_W, ROI_H
     
     # Menyiapkan kamera dengan konfigurasi standar yang dipakai sistem utama (640x480)
-    cap = setup_camera(fps=30, width=1280, height=720)
+    cap = setup_camera(fps=30, width=720, height=1280)
     
     if not cap.isOpened():
         print("[ERROR] Kamera tidak dapat dibuka!")
